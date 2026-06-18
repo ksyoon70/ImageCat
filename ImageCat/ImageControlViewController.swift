a9a9a7c39e7278172d677ff1ea0b1f0d0013bf6a
윤경섭 <ksyoon70@gmail.com>
2026-06-18 17:56:38 +0900

    [M003 커스텀뷰 및 리셋 버튼]

--- ImageCat/ImageControlViewController.swift
+++ ImageCat/ImageControlViewController.swift
@@ -10,10 +10,17 @@ import Cocoa
 class ImageControlViewController: NSViewController {
 
     @IBOutlet weak var curveControl: ImageCurveControl!
+    @IBOutlet weak var resetButton: NSButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
     }
-    
+    // 버튼을 누를 때 실행될 함수
+    @IBAction func resetButtonClicked(_ sender: NSButton) {
+        print("Curve 초기화 버튼이 클릭되었습니다.")
+        
+        // 여기에 초기화 관련 로직을 작성하세요.
+        // 예: 그래프 데이터를 리셋하거나 화면을 다시 그리는 코드
+    }
 }
